0109f030745c2ded5e0c4c0df739006d9357bb42
OnePuchPotato <zhibek.akylbekkyzy@alatoo.edu.kg>
2023-03-23 01:01:19 +0600

    Editing AnimeListServer and Anime class

--- anime/src/main/java/com/list/anime/service/AnimeListServer.java
+++ anime/src/main/java/com/list/anime/service/AnimeListServer.java
@@ -1,7 +1,29 @@
 package com.list.anime.service;
 
+import com.list.anime.domain.Anime;
+import com.list.anime.repository.AnimeListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AnimeListServer {
+    @Autowired
+    private AnimeListRepository repo;
+    public List<Anime> listAll() {
+        return repo.findAll();
+    }
+
+    public void save(Anime std) {
+        repo.save(std);
+    }
+
+    public Anime get(long id) {
+        return repo.findById(id).get();
+    }
+
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
 }
